c620025ad2b31f6ba8ec351775d209c832736066
frontal24 <ashlyssg@gmail.com>
2020-03-22 00:51:29 -0400

    First set of problems

--- labs109.py
+++ labs109.py
@@ -19,3 +19,50 @@ def ryerson_letter_grade(n):
     else:
         adjust = ""
     return "DCB"[tens - 5] + adjust
+
+#Ascending list
+def is_ascending(items):
+    if len(items) == 0:
+        return True
+    cur = items[0]
+    for item in items[1:]:
+        if cur > item:
+            return False
+        cur = item
+    return True
+
+# Riffle
+def riffle(items, out = True):
+    result=[]
+    items = items[:]
+    if len(items) == 0:
+        return []
+    #print(items)
+    a = int(len(items)/2)
+    list1 = items[:a]
+    list2 = items[a:]
+    for i in range(a):
+        if out:
+            result.extend([list1[i],list2[i]])
+        else:
+            result.extend([list2[i],list1[i]])
+    return result
+
+
+# Only odd digits
+def only_odd_digits(n):
+    while(n>10):
+        digit = n%10
+        n = n//10
+        if (digit%2==0):
+            return False
+    if (n%2==1):
+        return True
+    return False
+
+# Blocks in pyramid
+def pyramid_blocks(n, m, h):
+    result = 0
+    for i in range(h):
+        result += (n+i)*(m+i)
+    return result
